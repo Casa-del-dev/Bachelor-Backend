59bdc47f72ad6b8d3d6673b305ee313d30a1ffdb
Eren Homburg <ehomburg@student.ethz.ch>
2025-04-12 16:32:09 +0200

    added server style file-explorer structure

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,3 +4,4 @@ export { default as openai } from './openaiServer';
 export { default as openai2 } from './OpenAI_Check';
 export { default as openai3 } from './OpenAI_FromEditor';
 export { default as openai4 } from './OpenAI_FromTree';
+export { default as fileExplorer } from './problem';
