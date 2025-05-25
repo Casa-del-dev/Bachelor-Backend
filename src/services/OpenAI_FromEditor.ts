f56c1eddfbef444866a97b825735e4e995f2e3d6
Eren Homburg <ehomburg@student.ethz.ch>
2025-05-26 00:32:06 +0200

    fixing this prompt From editor

--- src/services/OpenAI_FromEditor.ts
+++ src/services/OpenAI_FromEditor.ts
@@ -60,7 +60,7 @@ The resulting JSON must represent a **semantically complete solution** to the Pr
 		Eg.'''		# Step 1					# Step 1
 					x = 1						x = 1
 
-												# Step 2
+												# MISSING STEP
 					x = x * x + x 		-> 		x = x * x + x
 
 					# Step 2					# Step 3
