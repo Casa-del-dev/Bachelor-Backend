4df917b3cdffc1b7751eaf35c6b238b9ea4c0cd1
Eren Homburg <ehomburg@student.ethz.ch>
2025-05-30 17:19:27 +0200

    saving them in the correct place

--- src/util/AbstractionStorage.ts
+++ src/util/AbstractionStorage.ts
@@ -7,7 +7,7 @@ export async function saveAbstractionInbetween(
 	abstractionId: string,
 	payload: AbstractionPayload
 ) {
-	const key = `${userId}.${problemId}.abstractionInbetween.${abstractionId}.json`;
+	const key = `${userId}/${problemId}/abstractionInbetween/${abstractionId}.json`;
 	await env.problemTree.put(key, JSON.stringify(payload), {
 		httpMetadata: { contentType: 'application/json' },
 	});
@@ -19,7 +19,7 @@ export async function loadAbstractionInbetween(
 	problemId: string,
 	abstractionId: string
 ): Promise<AbstractionPayload | null> {
-	const key = `${userId}.${problemId}.abstractionInbetween.${abstractionId}.json`;
+	const key = `${userId}/${problemId}/abstractionInbetween/${abstractionId}.json`;
 	const obj = await env.problemTree.get(key);
 	if (!obj) return null;
 	return (await obj.json()) as AbstractionPayload;
