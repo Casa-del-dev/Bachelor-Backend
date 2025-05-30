d535e07882948f380038cc40da6bd8ed91eb293c
Eren Homburg <ehomburg@student.ethz.ch>
2025-05-30 15:39:05 +0200

    updated where it the abstractionBetween should be saved

--- src/util/AbstractionStorage.ts
+++ src/util/AbstractionStorage.ts
@@ -1,12 +1,12 @@
 import { Step } from '../types';
 
 export async function saveAbstractionInbetween(env: Env, userId: string, problemId: string, abstractionId: string, steps: Step[]) {
-	const key = `${userId}.${problemId}.abstractionInbetween.${abstractionId}.json`;
+	const key = `${userId}/${problemId}/abstractionInbetween/${abstractionId}.json`;
 	await env.problemTree.put(key, JSON.stringify(steps), { httpMetadata: { contentType: 'application/json' } });
 }
 
 export async function loadAbstractionInbetween(env: Env, userId: string, problemId: string, abstractionId: string): Promise<Step[] | null> {
-	const key = `${userId}.${problemId}.abstractionInbetween.${abstractionId}.json`;
+	const key = `${userId}/${problemId}/abstractionInbetween/${abstractionId}.json`;
 	const obj = await env.problemTree.get(key);
 	if (!obj) return null;
 	return (await obj.json()) as Step[];
